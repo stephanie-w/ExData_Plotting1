e3fd033d1cc43aa8072f28251b34e0b5a4069ab4
stephanie-w <stephanie.w@noos.fr>
2015-05-10 11:34:01 +0200

    Fixing paths

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
-initial <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep=";", header=TRUE, nrows=100, na.strings = "?")
+initial <- read.table("household_power_consumption.txt", sep=";", header=TRUE, nrows=100, na.strings = "?")
 classes <- sapply(initial, class)
 colNames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep=";", header=FALSE, nrows=2880, comment.char="", colClasses=classes, na.strings = "?", skip=66637, col.names=colNames)
+data <- read.table("household_power_consumption.txt", sep=";", header=FALSE, nrows=2880, comment.char="", colClasses=classes, na.strings = "?", skip=66637, col.names=colNames)
 
 png('plot1.png', width=480, height=480, units="px")
 hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
--- plot3.R
+++ plot3.R
@@ -1,10 +1,10 @@
 #Changing the LC_TIME local, so days are in English (Windows)
 #Sys.setlocale("LC_TIME", "English")
 
-initial <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep=";", header=TRUE, nrows=100, na.strings = "?")
+initial <- read.table("household_power_consumption.txt", sep=";", header=TRUE, nrows=100, na.strings = "?")
 classes <- sapply(initial, class)
 colNames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep=";", header=FALSE, nrows=2880, comment.char="", colClasses=classes, na.strings = "?", skip=66637, col.names=colNames)
+data <- read.table("household_power_consumption.txt", sep=";", header=FALSE, nrows=2880, comment.char="", colClasses=classes, na.strings = "?", skip=66637, col.names=colNames)
 
 data$DateTime <- paste(data$Date, data$Time, sep=" ")
 data$DateTime <- strptime(data$DateTime, "%d/%m/%Y %H:%M:%S", tz=Sys.timezone())
